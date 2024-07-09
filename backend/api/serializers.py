9eb02149d900075eed012697c182c70da1b756f7
lostqubit <sg_suryaprakash@yahoo.com>
2024-07-09 20:03:40 +0530

    add product serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
-from .models import Source
+from .models import Source, Product
 
 
 class SourceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Source
         fields = "__all__"
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        exclude = ["source"]
+
+    def create(self, validated_data):
+        return Product.objects.create(**validated_data, source=self.context["source"])
