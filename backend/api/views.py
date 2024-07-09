855dca871c66efe3caef3e5cf2422f3b3ed71598
lostqubit <sg_suryaprakash@yahoo.com>
2024-07-09 20:04:10 +0530

    add product CRUD views

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,8 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import Http404
-from .models import Source
-from .serializers import SourceSerializer
+from .models import Source, Product
+from .serializers import SourceSerializer, ProductSerializer
 
 
 class SourceList(APIView):
@@ -21,26 +21,75 @@ class SourceList(APIView):
 
 
 class SourceDetail(APIView):
-    def get_object(self, id):
+    def get_object(self, s_id):
         try:
-            return Source.objects.get(id=id)
+            return Source.objects.get(id=s_id)
         except:
             raise Http404
 
-    def get(self, request, id):
-        source = self.get_object(id=id)
+    def get(self, request, s_id):
+        source = self.get_object(s_id=s_id)
         serializer = SourceSerializer(instance=source)
         return Response(serializer.data)
 
-    def put(self, request, id):
-        source = self.get_object(id=id)
+    def put(self, request, s_id):
+        source = self.get_object(id=s_id)
         serializer = SourceSerializer(instance=source, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, id):
-        source = self.get_object(id=id)
+    def delete(self, request, s_id):
+        source = self.get_object(id=s_id)
         source.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ProductList(APIView):
+    def get_source(self, s_id):
+        try:
+            return Source.objects.get(id=s_id)
+        except:
+            raise Http404
+
+    def get(self, request, s_id):
+        source = self.get_source(s_id=s_id)
+        products = source.product_set.all()
+        serializer = ProductSerializer(instance=products, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, s_id):
+        source = self.get_source(s_id=s_id)
+        serializer = ProductSerializer(
+            data=request.POST, context={"source": source})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProductDetail(APIView):
+    def get_product(self, p_id):
+        try:
+            return Product.objects.get(id=p_id)
+        except:
+            raise Http404
+
+    def get(self, request, s_id, p_id):
+        product = self.get_product(p_id=p_id)
+        serializer = ProductSerializer(instance=product)
+        return Response(serializer.data)
+
+    def put(self, request, s_id, p_id):
+        product = self.get_product(p_id=p_id)
+        serializer = ProductSerializer(instance=product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, s_id, p_id):
+        product = self.get_product(p_id=p_id)
+        product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
