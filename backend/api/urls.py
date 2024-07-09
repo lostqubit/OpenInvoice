550e839a7e669da18d815eb4e39e086bd1d68d8a
lostqubit <sg_suryaprakash@yahoo.com>
2024-07-09 20:04:29 +0530

    add endpoints for product CRUD

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path("source/", views.SourceList.as_view()),
-    path("source/<int:id>/", views.SourceDetail.as_view()),
+    path("source/<int:s_id>/", views.SourceDetail.as_view()),
+    path("source/<int:s_id>/products/", views.ProductList.as_view()),
+    path("source/<int:s_id>/products/<int:p_id>/",
+         views.ProductDetail.as_view()),
 ]
