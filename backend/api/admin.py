f025b8948dddf2b438681ebdf73ec895da2e3774
lostqubit <sg_suryaprakash@yahoo.com>
2024-07-09 20:03:27 +0530

    register models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Source, Product, Customer
 
-# Register your models here.
+admin.site.register(Source)
+admin.site.register(Product)
+admin.site.register(Customer)
