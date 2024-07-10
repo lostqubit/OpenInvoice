664d0b07b0496583bca8f1a7bf1d18076ba5de9d
lostqubit <sg_suryaprakash@yahoo.com>
2024-07-10 19:53:34 +0530

    add order serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Source, Product, Customer
+from .models import Source, Product, Customer, Order, OrderItem
 
 
 class SourceSerializer(serializers.ModelSerializer):
@@ -21,3 +21,39 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = "__all__"
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        exclude = ["order"]
+        extra_kwargs = {"updated": {"read_only": True},
+                        "created": {"read_only": True}}
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = ["id", "customer", "status", "handling_fee",
+                  "tax", "updated", "created", "items"]
+        extra_kwargs = {"updated": {"read_only": True},
+                        "created": {"read_only": True}}
+
+    def create(self, validated_data):
+        items_data = validated_data.pop("items")
+        order = Order.objects.create(**validated_data)
+        for item_data in items_data:
+            OrderItem.objects.create(**item_data, order=order)
+        return order
+
+    def update(self, instance, validated_data):
+        items_data = validated_data.pop('items')
+        instance = super().update(instance, validated_data)
+
+        instance.items.all().delete()
+        for item_data in items_data:
+            OrderItem.objects.create(**item_data, order=instance)
+
+        return instance
