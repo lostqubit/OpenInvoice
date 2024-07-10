908ba66addc808cbfd5182d393152658f0c0864f
lostqubit <sg_suryaprakash@yahoo.com>
2024-07-10 19:52:54 +0530

    register models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
-from .models import Source, Product, Customer
+from .models import Source, Product, Customer, Order, OrderItem
 
-admin.site.register(Source)
-admin.site.register(Product)
-admin.site.register(Customer)
+
+class SourceAdmin(admin.ModelAdmin):
+    readonly_fields = ("id",)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ("id",)
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    readonly_fields = ("id",)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ("id",)
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    readonly_fields = ("id",)
+
+
+admin.site.register(Source, SourceAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
